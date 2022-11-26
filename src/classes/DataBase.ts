db414e31f6db665b544178b2721ed7e70bb087bb
Brandonhsz <brandonhszzz@gmail.com>
2022-11-26 07:40:08 -0600

    singleton added to dabase class and added sequelize with connection test

--- src/classes/DataBase.ts
+++ src/classes/DataBase.ts
@@ -1,9 +1,30 @@
-
+import { Sequelize } from "sequelize";
 export class DataBase {
-  private instance: DataBase;
-
+  private static instance: DataBase;
+  private sequelize: Sequelize
   private constructor() {
-    null;
+    this.sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_NAME, process.env.DATABASE_PASS, {
+      "host": "localhost",
+      "dialect": "postgres"
+    })
+
+    this.tryConnection();
+  }
+
+  public static getInstance(): DataBase {
+    if (!DataBase.instance) {
+      DataBase.instance = new DataBase()
+    }
+    return DataBase.instance
+  }
+
+  private async tryConnection(): Promise<void> {
+    try {
+      await this.sequelize.authenticate();
+      console.log('Connection succesfully')
+    } catch (e) {
+      console.error('Unable to connect to the DataBase')
+    }
   }
 
 }
